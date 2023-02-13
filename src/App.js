dc68509bdb7f96b8c769e58355e58740edf4844d
Aarya Chopkar <aaryachopkar@gmail.com>
2023-02-13 19:07:33 +0530

    Auth Context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import SignUp from './components/SignUp';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <></>
+    <AuthProvider>
+      <Container 
+      className='d-flex align-items-center justify-content-center'
+      style={{minHeight: "100vh"}}
+      >
+        <div className="w-100" style={{minWidth:"400px"}}>
+          <SignUp/>
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 
