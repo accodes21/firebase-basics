ef8aea94f90c55c69c085a64b2e7adf3af588551
Aarya Chopkar <aaryachopkar@gmail.com>
2023-02-26 22:02:30 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { Container } from 'react-bootstrap';
 import './App.css';
 import SignUp from './components/SignUp';
 import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import LogIn from './components/LogIn';
+import ForgotPassword from './components/ForgotPassword';
 
 function App() {
   return (
@@ -11,7 +15,16 @@ function App() {
       style={{minHeight: "100vh"}}
       >
         <div className="w-100" style={{minWidth:"400px"}}>
-          <SignUp/>
+          <BrowserRouter>
+            <AuthProvider>
+              <Routes>
+                <Route exact path='/' element={<Dashboard/>}/>
+                <Route path='/signup' element={<SignUp/>} />
+                <Route path='/login' element={<LogIn/>} />
+                <Route path='/forgot-password' element={<ForgotPassword/>} />
+              </Routes>
+            </AuthProvider>
+          </BrowserRouter>
         </div>
       </Container>
     </AuthProvider>
